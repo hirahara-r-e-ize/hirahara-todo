0c8f64ad346b3203c8ea207a0c28fa41776f50d3
hirahara-r-e-ize <r.hirahara@e-ize.jp>
2021-01-13 02:12:19 +0900

    Todoの編集、新規作成用のビュー作成

--- todo/urls.py
+++ todo/urls.py
@@ -3,4 +3,6 @@ from . import views
 urlpatterns = [
     path('', views.todo_list, name='todo_list'),
     path('<int:pk>/', views.todo_detail, name='todo_detail'),
+    path('add/', views.todo_add, name='todo_add'),
+    path('<int:pk>/edit/', views.todo_edit, name='todo_edit'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Todo
+from .forms import TodoForm
+from django.shortcuts import redirect
 
 # Create your views here.
 def todo_list(request):
@@ -10,3 +12,30 @@ def todo_list(request):
 def todo_detail(request, pk):
     todo = get_object_or_404(Todo, pk=pk)
     return render(request, 'todo/todo_detail.html', {'todo': todo})
+
+def todo_add(request):
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.updated_date = timezone.now()
+            post.save()
+            return redirect('todo_detail', pk=post.pk)
+    else:
+        form = TodoForm()
+    return render(request, 'todo/todo_edit.html', {'form': form})
+
+def todo_edit(request, pk):
+    post = get_object_or_404(Todo, pk=pk)
+    if request.method == "POST":
+        form = TodoForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.updated_date = timezone.now()
+            post.save()
+            return redirect('todo_detail', pk=post.pk)
+    else:
+        form = TodoForm(instance=post)
+    return render(request, 'todo/todo_edit.html', {'form': form})
