ed7627cd18262a96da9ffc21b0848f3ba2d31ab6
hirahara-r-e-ize <r.hirahara@e-ize.jp>
2021-01-13 01:07:41 +0900

    ビュー、テンプレート、CSSを追加

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Todo
 
@@ -6,3 +6,7 @@ from .models import Todo
 def todo_list(request):
     todos = Todo.objects.filter(updated_date__lte=timezone.now()).order_by('updated_date')
     return render(request, 'todo/todo_list.html', {'todos': todos})
+
+def todo_detail(request, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+    return render(request, 'todo/todo_detail.html', {'todo': todo})
