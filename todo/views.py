c195822aa4e47aa0c9867be48390a65aa6b6b714
hirahara-r-e-ize <r.hirahara@e-ize.jp>
2021-01-12 19:25:06 +0900

    トップページの変更

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Todo
 
 # Create your views here.
 def todo_list(request):
-    return render(request, 'todo/todo_list.html', {})
+    todos = Todo.objects.filter(updated_date__lte=timezone.now()).order_by('updated_date')
+    return render(request, 'todo/todo_list.html', {'todos': todos})
