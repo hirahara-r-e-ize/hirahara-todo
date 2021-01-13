0f188d6383a65685d22d7472a9a95d392a170411
hirahara-r-e-ize <r.hirahara@e-ize.jp>
2021-01-13 20:49:41 +0900

    URLの変更

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,7 +19,7 @@ from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/login/', views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('todo.urls')),
 ]
